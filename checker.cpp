65f9cb82e02259741b27d01b10d257fd2145c69a
Nerazzurri <110226856+Narezzurri@users.noreply.github.com>
2026-08-26 10:23:37 +0800

    Fix Server ip address not found error

--- checker.cpp
+++ checker.cpp
@@ -9,21 +9,25 @@ signed main (signed argc, const char* argv[])
 {
 	for (int i = 1; i < argc; i++)
 	{
+		printf ("%s: ", argv[i]);
 		freopen (argv[i], "r", stdin);
-		cin >> s;
-		while (s != "remote")
-			cin >> s;
-		scanf ("%d.%d.%d.%d", &a ,&b, &c, &d);
-		s = argv[i];
-		if (s.length() < 5 || s.substr(s.length() - 5) != ".ovpn")
-			printf ("%s is of an unsupported file type.", argv[i]);
+		while (cin >> s && s != "remote");
+		if (s != "remote")
+			puts ("Server ip address not found.");
 		else
 		{
-			sprintf (cmd, "start \"%s\" cmd /c \"ping -t %d.%d.%d.%d\"\n", s.substr(s.find_last_of('\\') + 1).data(), a, b, c, d);
-			system (cmd);
+			scanf ("%d.%d.%d.%d", &a ,&b, &c, &d);
+			s = argv[i];
+			if (s.length() < 5 || s.substr(s.length() - 5) != ".ovpn")
+				puts ("Unsupported file type.");
+			else
+			{
+				sprintf (cmd, "start \"%s\" cmd /c \"ping -t %d.%d.%d.%d\"\n", s.substr(s.find_last_of('\\') + 1).data(), a, b, c, d);
+				system (cmd);
+				puts ("Done.");
+			}
 		}
 		fclose (stdin);
 	}
-	puts ("Done.");
 	return 0;
 }
