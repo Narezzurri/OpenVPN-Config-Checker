a8aeb413d7ad6ae1095e49c2774911e07f9fee65
Nerazzurri <110226856+Narezzurri@users.noreply.github.com>
2026-08-26 11:03:39 +0800

    Add support for hostname&Fix multifile input error

--- checker.cpp
+++ checker.cpp
@@ -10,24 +10,27 @@ signed main (signed argc, const char* argv[])
 	for (int i = 1; i < argc; i++)
 	{
 		printf ("%s: ", argv[i]);
-		freopen (argv[i], "r", stdin);
-		while (cin >> s && s != "remote");
-		if (s != "remote")
-			puts ("Server ip address not found.");
+		s = argv[i];
+		if (s.length() < 5 || s.substr(s.length() - 5) != ".ovpn")
+			puts ("Unsupported file type.");
 		else
 		{
-			scanf ("%d.%d.%d.%d", &a ,&b, &c, &d);
-			s = argv[i];
-			if (s.length() < 5 || s.substr(s.length() - 5) != ".ovpn")
-				puts ("Unsupported file type.");
+			cin.clear();
+			freopen (argv[i], "r", stdin);
+			cin >> s;
+			while (cin >> s && s != "remote");
+			if (s != "remote")
+				puts ("Server ip address not found.");
 			else
 			{
-				sprintf (cmd, "start \"%s\" cmd /c \"ping -t %d.%d.%d.%d\"\n", s.substr(s.find_last_of('\\') + 1).data(), a, b, c, d);
+				cin >> s;
+				string t = argv[i];
+				sprintf (cmd, "start \"%s\" cmd /c \"ping -t %s\"\n", t.substr(t.find_last_of('\\') + 1).data(), s.data());
 				system (cmd);
 				puts ("Done.");
 			}
+			// fclose (stdin);
 		}
-		fclose (stdin);
 	}
 	return 0;
 }
