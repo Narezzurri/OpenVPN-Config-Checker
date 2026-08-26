a07f6722db619c92865817cfea1c5e94f6add5b4
Narezzurri <hacker_mickey@outlook.com>
2026-08-27 00:04:36 +0800

    Add command line parameter "/t" "/v" to customize the efficiency of process

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
-# OpenVPN-Config-Checker
+# OpenVPN-Config-Checker 
 
-A practical software to check in batch the connection between the host and remote VPN server,which is recorded in the OpenVPN config file (\*.ovpn).
+A practical Win32 software to check in batch the connection between the host and remote VPN server,which is recorded in the OpenVPN config file (\*.ovpn).
 
-## Function
+## Function 
 
-- Check if the ip address is available to be connected.(hostname is temporarily unsupported)
+- Check if the ip address is available to be connected.(hostname is temporarily unsupported) 
 
-## Usage
+## Usage 
 
-`> checker config1.ovpn config2.ovpn ...` 
+`> checker <param1> <param2> config1.ovpn config2.ovpn ...` 
 
-or drag the target config file onto checker.exe
+### Parameters 
+
+- `/v speed` 
+
+	Customize the efficiency,that is,the number of valid files to process at once.
+	
+	Set `0` to process all files at once,default is `1`.
+
+	**Warning**: Process too many files at once may cause the system to crash.
+
+- `/t time` 
+
+	Customize the time to break after process a bulk of files which is set above.
+	
+	Set any negative numbers to process the files one by one without starting a new window,which is default
+
+In addition, you can drag the target config file onto `checker.exe` to start the programme by default settings.
+
+## Upcoming Features 
+
+- Generate a list of available server ip address.
+
+- Customize the threshold of ping timeout.
--- checker.cpp
+++ checker.cpp
@@ -1,14 +1,52 @@
+#include	<windows.h>
 #include	<iostream>
+#include	<vector>
 using namespace std;
+using db = double;
 
 string s;
+int T = -1;
 char cmd[300];
 int a, b, c, d;
+db sec_per_wait;
 
 signed main (signed argc, const char* argv[])
 {
-	for (int i = 1; i < argc; i++)
+	vector<int> mark (argc);
+	mark[0] = 1;
+	for (int i = 1; i < argc; i++) if (argv[i][0] == '/') [&] ()
 	{
+		mark[i] = 1;
+		if (i == argc - 1 || [&] () -> bool
+		{
+			switch (argv[i][1])
+			{
+				case 't':
+					if (sscanf (argv[++i], "%lf", &sec_per_wait) <= 0)
+						return 1;
+					break;
+				case 'v':
+					if (sscanf (argv[++i], "%d", &T) <= 0)
+						return 1;
+					break;
+				default:
+					printf ("Unrecognized parameter : %s.Skip\n", argv[i]);
+			}
+			return 0;
+		}())
+		{
+			printf ("Miss parameter for argument %d : %s.Skip\n", i - 1, argv[i - 1]);
+			if (i == argc - 1)
+				return ;
+			i--;
+		}
+		mark[i] = 1;
+	} ();
+	int cnt = 0;
+	for (int i = 1; i < argc; i++) if (!mark[i])
+	{
+		if (T > 0 && cnt && !(cnt % T))
+			Sleep (sec_per_wait * 1000);
 		printf ("%s: ", argv[i]);
 		s = argv[i];
 		if (s.length() < 5 || s.substr(s.length() - 5) != ".ovpn")
@@ -24,13 +62,20 @@ signed main (signed argc, const char* argv[])
 			else
 			{
 				cin >> s;
-				string t = argv[i];
-				sprintf (cmd, "start \"%s\" cmd /c \"ping -t %s\"\n", t.substr(t.find_last_of('\\') + 1).data(), s.data());
+				string name = argv[i];
+				name = name.substr(name.find_last_of('\\') + 1).data();
+				if (T < 0)
+					sprintf (cmd, "title %s && ping %s -w 1000", name.data(), s.data());
+				else
+					sprintf (cmd, "start \"%s\" cmd /c \"ping -t %s -w 1000\"\n", name.data(), s.data());
 				system (cmd);
+				cnt++;
 				puts ("Done.");
 			}
-			// fclose (stdin);
 		}
 	}
+	cout << cnt << " files checked successfully" << endl;
+	freopen ("CON", "r", stdin);
+	cin >> s;
 	return 0;
 }
